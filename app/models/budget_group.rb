f8f7fb288a676f683d3a540bdd1f4bf95edcb623
josuerushanika <josuerushanika@gmail.com>
2023-06-26 19:30:09 +0200

    Update models according to the ERD diagram

--- app/models/budget_group.rb
+++ app/models/budget_group.rb
@@ -1,2 +1,4 @@
 class BudgetGroup < ApplicationRecord
+    belongs_to :group
+    belongs_to :budget
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+    has_many :budget_groups
+    has_many :budgets, through: :budget_groups
+    belongs_to :author, foreign_key: :author_id, class_name: 'User'
 end
