68a8c886269e291253c5dbb4da4b7282b54eefa6
josuerushanika <josuerushanika@gmail.com>
2023-06-27 20:33:49 +0200

    Update Controller router and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def index     
+    end
+
+    def show
+        if params[:id] == 'sign_out'
+          sign_out_and_redirect
+        else
+          find_user
+        end
+      end
+
+    private
+
+  def sign_out_and_redirect
+    sign_out current_user
+    redirect_to authenticated_root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [:index, :show] 
   resources :budget
   resources :groups
 
@@ -8,4 +10,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    get '/', to: redirect('/splash')
+  end
 end
