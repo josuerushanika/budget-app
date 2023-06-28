fa04330889b6ae9414ecbdcca0cbb961bc40ed63
josuerushanika <josuerushanika@gmail.com>
2023-06-28 11:03:38 +0200

    Fix some bugs

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -5,20 +5,29 @@ class BudgetsController < ApplicationController
    @budgets = @group.budgets.includes(:budget_groups).order(created_at: :desc)
   end 
 
-  def new
-    @budget = Budget.new
-    @groups = Group.find(params[:params_id])
+  # def new
+  #   @budget = Budget.new
+  #   @group = Group.find(params[:group_id])
 
-    return unless @groups.nil
-    @group = []
-  end
+  #   return unless @groups.nil
+  #   @group = []
+  # end
 
   def create
     @budget = Budget.new(budget_params)
     @budget.author_id = current_user.id 
 
     if @budget.save
-      @budget_group = BudgetGroup.new(budget_id: @b)
+      @budget_group = BudgetGroup.new(budget_id: @budget.id, group_id: params[:group_id])
+      redirect_to user_group_budgets_path(current_user) if @budget_group.save
+    else
+      render :new
+    end    
   end
 
+  private
+
+  def budget_params
+    params.require(:budget).permit(:name, :amount)
+  end
 end
